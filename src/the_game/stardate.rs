efa8bcfab697f407edf3fb6841ddf7e0af96c1a5
Evan Williams <ejw@fig.com>
2021-01-22 23:02:05 -0500

    Comment Cleaningg

--- src/the_game/stardate.rs
+++ src/the_game/stardate.rs
@@ -17,7 +17,6 @@ impl Display for StarDate {
 }
 
 impl<I: Into<i32>> ops::Add<I> for StarDate {
-    //u16
     type Output = StarDate;
 
     fn add(self, rhs: I) -> StarDate {
@@ -26,16 +25,15 @@ impl<I: Into<i32>> ops::Add<I> for StarDate {
 }
 
 impl<I: Into<i32>> ops::AddAssign<I> for StarDate {
-    //u16
     fn add_assign(&mut self, rhs: I) {
         *self = StarDate(self.0 + rhs.into());
     }
 }
 
 impl ops::Sub<StarDate> for StarDate {
-    type Output = i32; //u16
+    type Output = i32;
 
     fn sub(self, rhs: StarDate) -> i32 {
         self.0 - rhs.0
-    } //u16
+    }
 }
